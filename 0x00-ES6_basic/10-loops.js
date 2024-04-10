1fad0ade6bfa11fac78fee956c2b6bd169ba2e63
AyaTarek95 <aya.tarek213@icloud.com>
2024-04-10 18:39:18 +0300

    task10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+  let result = []
+  for (let idx of array) {
+    result.push(appendString + idx)
+  }
+
+  return result;
+}
