f132be7683b5cf9d406e4ae40d8c574574ca8265
AyaTarek95 <aya.tarek213@icloud.com>
2024-04-18 00:56:40 +0300

    task4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,6 +16,33 @@ interface printTeacherF {
 }
 
 const  printTeacher: printTeacherF = (firstName, lastName) => {
-  return `${firstName[0]}.${lastName}`
+  return `${firstName[0]}. ${lastName}`
+}
+
+interface studentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+interface  studentInterface{
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements studentInterface{
+  firstName: string
+  lastName: string
+
+  constructor ({ firstName, lastName }: studentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+}
 }
 
